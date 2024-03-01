186ee1811c34d8c51924d44622b8ab74b6544b7f
Felipe Seixas <felipe.economista@hotmail.com>
2024-03-01 09:08:06 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dados do Servidor</title>
+</head>
+<body>
+    <h1>Exercício 2 - Dados do Servidor (phpinfo())</h1>
+    <?php phpinfo()?>
+</body>
+</html>
